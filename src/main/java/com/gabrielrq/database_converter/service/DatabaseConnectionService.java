a48b358dc22ff2c38d0921e9e9489d3fc70bc582
Gabriel-RQ <123425138+Gabriel-RQ@users.noreply.github.com>
2025-10-07 11:08:10 -0300

    feat: adds database connection factory service

---
+++ src/main/java/com/gabrielrq/database_converter/service/DatabaseConnectionService.java
@@ -0,0 +1,33 @@
+package com.gabrielrq.database_converter.service;
+
+import com.gabrielrq.database_converter.dto.DbConnectionConfigDTO;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.stereotype.Service;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@Service
+public class DatabaseConnectionService {
+
+    public DataSource createDataSource(DbConnectionConfigDTO config) {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl(config.jdbcUrl());
+        dataSource.setUsername(config.username());
+        dataSource.setPassword(config.password());
+        dataSource.setDriverClassName(config.driverClassName());
+        return dataSource;
+    }
+
+    public JdbcTemplate createJdbcTemplate(DbConnectionConfigDTO config) {
+        DataSource dataSource = createDataSource(config);
+        return new JdbcTemplate(dataSource);
+    }
+
+    public Connection createConnection(DbConnectionConfigDTO config) throws SQLException {
+        return createDataSource(config).getConnection();
+    }
+
+}
