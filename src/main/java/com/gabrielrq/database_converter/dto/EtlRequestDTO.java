64a77d72d2fd5f290fe26f173cce3401da3e367a
Gabriel-RQ <123425138+Gabriel-RQ@users.noreply.github.com>
2025-11-10 16:13:07 -0300

    rename: EtlRequest to EtlRequestDTO

--- src/main/java/com/gabrielrq/database_converter/dto/EtlRequestDTO.java
+++ src/main/java/com/gabrielrq/database_converter/dto/EtlRequestDTO.java
@@ -2,7 +2,7 @@ package com.gabrielrq.database_converter.dto;
 
 import java.util.UUID;
 
-public record EtlRequest(
+public record EtlRequestDTO(
         UUID id,
         DbConnectionConfigDTO config,
         String target
